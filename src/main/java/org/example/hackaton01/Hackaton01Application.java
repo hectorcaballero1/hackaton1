fbd26a5e5477e4752518ddb48088dbaf57c12060
hectorcaballero <hectorcaballero1205@gmail.com>
2025-10-21 18:14:49 -0500

    first commit

---
+++ src/main/java/org/example/hackaton01/Hackaton01Application.java
@@ -0,0 +1,13 @@
+package org.example.hackaton01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackaton01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hackaton01Application.class, args);
+    }
+
+}
